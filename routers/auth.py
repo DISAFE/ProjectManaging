0491e32043338924274df24b7e8ae05b90b09d19
disafe <kangkyubin3@gmail.com>
2025-08-15 13:25:36 +0900

    feat(auth): Defining the endpoint schema
    - define endpoint schema:  /signup, /login

---
+++ routers/auth.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, status, Depends, Body
+from typing import Annotated
+
+from pydantic import EmailStr
+
+from schemata import UserCreate
+import jwt
+import bcrypt
+from database import SessionDep
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+    dependencies=[],
+)
+
+@router.post("/signup", responses=status.HTTP_201_CREATED)
+async def sign_up(
+        usercreate: Annotated[UserCreate, Body()],
+        session: SessionDep
+):
+    """
+    :param usercreate: UserCreate model
+    :param session: Database session
+
+    Steps:
+    1. check if usercreate.email or usercreate.username exists in DB
+        True :raise status.HTTP_409_CONFLICT
+    2. hash usercreate.password
+    3. convert usercreate to user(User)
+    4. create new User Record in DB
+    :return: {"success": "ok"}
+    """
+
+@router.post("/login", response=status.HTTP.)
+async def login(
+        email: Annotated[EmailStr, Body()],
+        password: Annotated[str, Body()],
+        session: SessionDep
+):
+    """
+    :param email:
+    :param password:
+    :param session: Database session
+
+    steps:
+    1. Query the User table for a record matching the email
+        :except status.HTTP_404_NOT_FOUND, :detail "email is not exist"
+    2. verify User.password = password by bcrypt
+        :except status.HTTP_404_NOT_FOUND :detail "password is wrong"
+    3. make payload
+    4. generate token
+    :return: token(by header)
+    """
